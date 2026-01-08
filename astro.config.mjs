02934d2a3eca85aca053ded158670efeeae01e8d
mhballin <46456219+mhballin@users.noreply.github.com>
2026-01-08 00:34:29 -0500

    CV update and profile photo umong other things

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
+  server: { host: '127.0.0.1' },
+  preview: { host: '127.0.0.1' },
   integrations: [mdx(), sitemap(), tailwind()]
 });
